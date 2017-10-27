e58653d9fee26e7382df4fc7d11dd4ac229fba5a
steph411 <etabalionel@gmail.com>
2017-10-27 19:33:28 +0100

    first commit with all the project files set up

---
+++ optima_backend/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/steph/Documents/COURSENSPT/programmation/Front_End_Web/WEB_Dev/Dev_Projects/Optima/src/optima_backend/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
